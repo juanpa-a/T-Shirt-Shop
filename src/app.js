39efb613f4450990d441d05c5fce11d9f37ea024
Juan Pa <jpabloascencioglez@gmail.com>
2019-06-18 15:13:14 -0500

    removed handlebars

--- src/app.js
+++ src/app.js
@@ -2,22 +2,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const exphbs = require('express-handlebars');
 const path = require('path');
 
 // Settings
 const port = process.env.PORT || 4000;
 app.set(port);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
-    extname: 'hbs',
-    helpers: require('./lib/handlebars.js')
-}));
-app.set('view engine', '.hbs');
-
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
